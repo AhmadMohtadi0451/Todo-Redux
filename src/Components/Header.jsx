596a5d133fb031328a9bff7022e36d282c26794d
Ahmad Mohtadi <ahmadmohtadi0451@gmail.com>
2023-01-29 16:22:44 +0330

    Header done.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
 
 const Header = () => {
   return (
-    <Grid>
-      <Grid>hello</Grid>
+    <Grid
+      display="flex"
+      justifyContent="center"
+      alignContent="center"
+      height="5rem"
+      bgcolor="rgb(37,100,207)"
+    >
+      <Typography
+        sx={{ fontSize: "2rem", alignSelf: "center", color: "white" }}
+      >
+        To Do
+      </Typography>
     </Grid>
   );
 };
